8702ba3f22bc70382e669e0ecb58324c106a4af2
adrees <madreesgujjar601@gmail.com>
2025-03-18 06:02:33 +0500

    Uodate backend

--- api/index.js
+++ api/index.js
@@ -4,14 +4,15 @@ const cors = require('cors')
 require('dotenv').config()
 const path = require('path');
 const port = process.env.PORT || 4000;
-const connectDB = require('./src/config/db')
-const authRouter = require('./src/routes/auth.route');
-const moviesRouter = require('./src/routes/movies.route');
-const userPermisionsRouter = require('./src/routes/userPermisions.route');
+const connectDB = require('../src/config/db')
+const authRouter = require('../src/routes/auth.route');
+const moviesRouter = require('../src/routes/movies.route');
+const userPermisionsRouter = require('../src/routes/userPermisions.route');
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, '/src/public')))
-app.use(express.json())
+app.use(express.json());
+app.get('/',(req,res)=>res.send("Movie List Api is working ...."));
 app.use('/auth', authRouter);
 app.use('/movies', moviesRouter);
 app.use('/permision', userPermisionsRouter);
